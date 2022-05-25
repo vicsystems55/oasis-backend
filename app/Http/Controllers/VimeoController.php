c0961e8557d50bafa9ba651e312cefdad978196c
vicSystems <victorasuquob@gmail.com>
2022-05-25 14:45:53 +0100

    vimeo integration

---
+++ app/Http/Controllers/VimeoController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Vimeo\Laravel\Facades\Vimeo;
+
+use Vimeo\Laravel\VimeoManager;
+
+class VimeoController extends Controller
+{
+    //
+
+    public function getMovies(Request $request)
+    {
+        
+        $data = Vimeo::request('/me/videos', ['per_page' => 10], 'GET');
+
+
+        return $data;
+    }
+
+    public function uploadVideo(Request $request, VimeoManager $vimeo)
+    {
+
+        $request->validate([
+            'video' => 'required|mimes:mp4,ogx,oga,ogv,ogg,webm|max:50000',
+            // 'amount' => 'required|numeric|min:99700|between:0,99.99',
+            // 'number_of_accounts' => 'required|numeric|min:1|max:15',
+            // 'featured_image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:50000',
+            
+        ]);
+
+        // dd($request->file('video')->getPath());
+
+
+        
+        // $data = Vimeo::request('/me/videos', ['per_page' => 10], 'GET');
+
+        // $data = Vimeo::upload($request->file('video'));
+
+        $data = $vimeo->upload($request->video,[
+            'name' => 'name',
+            'description' => 'description'
+        ]);
+
+        
+
+
+        return $data;
+    }
+
+
+}
---
+++ database/migrations/2022_05_24_142423_create_application_submissions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicationSubmissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('application_submissions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('application_submissions');
+    }
+}
